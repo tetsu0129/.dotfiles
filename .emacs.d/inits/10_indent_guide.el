e71310348227ef4509af07c9922d72c5e5d1894c
tetsu0129 <tetsu.0129@gmail.com>
2019-10-27 17:23:39 +0900

    add

---
+++ .emacs.d/inits/10_indent_guide.el
@@ -0,0 +1,6 @@
+(setq highlight-indent-guides-method 'column)
+(setq highlight-indent-guides-auto-odd-face-perc 15)
+(setq highlight-indent-guides-auto-even-face-perc 15)
+(setq highlight-indent-guides-auto-character-face-perc 20)
+(setq highlight-indent-guides-character ?\|)
+(setq highlight-indent-guides-responsive 'top)
